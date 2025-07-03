e503c88be92f011a14b2639842b3fc0af52e85e1
NMTIEN05 <nmtbao001@gmail.com>
2025-07-03 23:59:44 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// src/App.tsx
+import React, { useEffect } from 'react';
+import { useRoutes } from 'react-router-dom';
+import { routes } from './router';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import HomePage from './pages/Home/HomePage';
-import Products from './pages/Products/Products';
-import ProductDetail from './pages/Products/ProductDetail';
-import Services from './pages/Services/Services';
-import Contact from './pages/Contact/Contact';
-import Cart from './pages/Cart/Cart';
-import Shipping from './pages/Shipping/Shipping';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8888/api/ping")
+      .then((response) => {
+        console.log(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Lỗi khi gọi API:", error);
+      });
+  }, []);
+  const routing = useRoutes(routes);
+
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:slug" element={<ProductDetail />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/shipping" element={<Shipping />} />
-      </Routes>
-      <Footer />
+     
+      {routing}
+      
     </div>
   );
 }
